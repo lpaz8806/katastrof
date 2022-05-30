176564ceb8ea8f783d05c2878af47cd4a68613ec
Luis Paz <lpaz8806@gmail.com>
2022-05-30 09:47:30 +0200

    boilerplate

---
+++ src/main/java/se/jensen/katastrof/KatastrofApplication.java
@@ -0,0 +1,9 @@
+package se.jensen.katastrof;
+
+public class KatastrofApplication {
+
+    public static void main(String[] args) {
+        System.out.println("App Works!");
+    }
+
+}
---
+++ src/test/java/se/jensen/katastrof/KatastrofApplicationTests.java
@@ -0,0 +1,13 @@
+package se.jensen.katastrof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatastrofApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
